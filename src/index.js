d7d02541116ed5f00d478d4b99ba329ddbcc74a8
Вячеслав Федорцов <viachaslaufedartsou@gmail.com>
2020-10-01 00:33:52 +0300

    feat: add functional

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
-
 exports.min = function min (array) {
-  return 0;
-}
+  let minimum = Math.min.apply(null, array);  
+  if(array == undefined || array.length == 0) {
+    return 0;
+  } else {
+      return minimum;
+  }  
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+  let maximum = Math.max.apply(null, array);
+  if(array == undefined || array.length == 0) {
+    return 0;
+  } else {
+      return maximum;
+  }  
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  let sum = 0;  
+  let result = 0;
+  if(array == undefined || array.length == 0) {
+    return 0;
+  } else {
+       array.forEach(function(elem) {
+        sum += elem;  
+        result = sum / array.length;             
+  });
+  return result;      
+}};
+
+// exports.avg = function avg (array) {
+//   let sum = 0;  
+//   let result = 0;
+//   if(array == undefined || array.length == 0) {
+//     return 0;
+//   } else {
+//       array.reduce(function(sum, elem) {
+//         sum += elem;
+//         result = sum / array.length;
+//     }, 0);      
+//   };
+//   return result;
+// }; 
